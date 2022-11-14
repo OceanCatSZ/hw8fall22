a60836050cd5eee19051ed0e12c00d6da8066915
daniel.melanson <daniel.melanson@2dd36be4-16a9-7120-5af3-ce57b7db7bff>
2022-11-14 19:32:10 +0000

    feat: implement
    git-svn-id: https://github.com/umass-compsci-220/hw8-fall-22.git/trunk@2 2dd36be4-16a9-7120-5af3-ce57b7db7bff

---
+++ build.sh
@@ -0,0 +1,2 @@
+rm hw8-fall-22.zip
+zip hw8-fall-22.zip ./src/* ./include/* ./package-lock.json ./package.json
---
+++ src/interpreter.js
@@ -0,0 +1,17 @@
+function interpExpression(state, expr) {
+  // TODO
+}
+
+function interpStatement(state, stmt) {
+  // TODO
+}
+
+function interpProgram(stmts) {
+  // TODO
+}
+
+module.exports = {
+  interpExpression,
+  interpStatement,
+  interpProgram,
+};
