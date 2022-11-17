3ffd5c0c09efcf6efea1c7ee78449ae5ccf559a9
daniel.melanson <daniel.melanson@2dd36be4-16a9-7120-5af3-ce57b7db7bff>
2022-11-17 03:42:33 +0000

    fix: trim input and clarifications
    git-svn-id: https://github.com/umass-compsci-220/hw8-fall-22.git/trunk@6 2dd36be4-16a9-7120-5af3-ce57b7db7bff

--- src/interpreter.test.js
+++ src/interpreter.test.js
@@ -2,13 +2,13 @@ const assert = require("assert");
 const { parseExpression, parseProgram } = require("../include/parser");
 const { interpExpression, interpProgram, interpStatement } = require("./interpreter");
 
-test("multiplication with a variable", () => {
+test("interpExpression interprets multiplication with a variable", () => {
   const r = interpExpression({ x: 10 }, parseExpression("x * 2").value);
 
   assert(r === 20);
 });
 
-test("assignment", () => {
+test("interpProgram interprets basic declaration then assignment", () => {
   const st = interpProgram(parseProgram("let x = 10; x = 20;").value);
 
   assert(st.x === 20);
